f168312a31e7ccc94ce7cce95ec18a47431c789f
jeel tank <jeeltank.developer@gmail.com>
2026-05-23 21:05:32 +0530

    modified:   myapp/models.py

--- myapp/models.py
+++ myapp/models.py
@@ -288,4 +288,56 @@ class message(models.Model):
 
     def __str__(self):
         return f"{self.role} - {self.chat_room.sender.name}"
+    
+    def whenpublished(self):
+        now = timezone.now()
+        diff = now-self.created_at
+
+        if diff.days == 0 and diff.seconds >= 0 and diff.seconds < 60:
+            seconds = diff.seconds
+
+            if seconds == 1:
+                return str(seconds) + "second ago"
+            else:
+                return str(seconds) + " seconds ago"
+            
+        if diff.days == 0 and diff.seconds >= 60 and diff.seconds < 3600 :
+            minutes = math.floor(diff.seconds/60)
+             
+            if minutes == 1:
+                return str(minutes) + " minute ago"
+            else:
+                return str(minutes) + " minutes ago"
+            
+        if diff.days == 0 and diff.seconds >= 3600 and diff.seconds < 86400 :
+            hours = math.floor(diff.seconds/3600)
+
+            if hours == 1:
+                return str(hours) + " hour ago"
+            else:
+                return str(hours) + " hours ago"
+            
+        if diff.days >= 1 and diff.days < 30:
+            days = diff.days
+
+            if days == 1:
+                return str(days) + " day ago"
+            else:
+                return str(days) + " days ago"
+            
+        if diff.days >= 30 and diff.days < 365:
+            months = math.floor(diff.days/30)
+
+            if months == 1:
+                return str(months) + " month ago" 
+            else:
+                return str(months) + " months ago"
+            
+        if diff.days >= 365:
+            years = math.floor(diff.days/365)
+
+            if years == 1:
+                return str(years) + " year ago"
+            else:
+                return str(years) + " years ago"
 
